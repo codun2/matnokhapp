8474e1ed0def8bae3cdf37af226d65f55fd37d35
codun <dev@matnokh>
2026-09-26 11:59:34 +0200

    tajer: orange storefront launcher icon (matches family) + applicationId com.matnook.tajer for Play

--- tajer/app/build.gradle.kts
+++ tajer/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.matnokh.tajer"
+        applicationId = "com.matnook.tajer"
         minSdk = 26
         targetSdk = 36
         versionCode = 39
